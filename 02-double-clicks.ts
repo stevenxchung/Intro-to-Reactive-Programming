e10826919888de2d25dc87dc3d00c7e8165b48c8
Steven <stevenxchung@gmail.com>
2018-10-30 19:49:13 -0400

    Update after testing

--- 02-double-clicks.ts
+++ 02-double-clicks.ts
@@ -1,4 +1,22 @@
 // Detecting double clicks
 
+import { fromEvent, interval } from 'rxjs';
+import { buffer, throttle, map, filter } from 'rxjs/operators';
+
 let button = document.querySelector('button');
 let label = document.querySelector('h4');
+
+let clickStream = fromEvent(button, 'click');
+let doubleClickStream = clickStream.pipe(
+  buffer(clickStream.pipe(throttle(val => interval(250)))),
+  map(arr => arr.length),
+  filter(len => len === 2)
+);
+
+doubleClickStream.subscribe(event => {
+  label.textContent = 'Double Clicked!';
+});
+
+doubleClickStream.pipe(throttle(val => interval(1000))).subscribe(suggestion => {
+  label.textContent = 'First Try!';
+});
