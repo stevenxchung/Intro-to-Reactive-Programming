14c6627c597820fe2625c27f082af33f6db404df
Steven <stevenxchung@gmail.com>
2018-10-31 19:26:34 -0400

    Add observable streams

--- 03-why-rxjs.ts
+++ 03-why-rxjs.ts
@@ -8,4 +8,13 @@ console.log(b);
 
 a = 4;
 b = 11 * a;
+
 console.log(b)
+
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+let streamA = of(3, 4);
+let streamB = streamA.pipe(map(a => 10 * a));
+
+streamB.subscribe(b => console.log(b))
