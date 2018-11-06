b074fcaa50304f13820f19fe8e987a08011e717c
Steven <stevenxchung@gmail.com>
2018-11-06 18:26:35 -0500

    Testing feature to fetch data

--- rxjs-test/src/index.ts
+++ rxjs-test/src/index.ts
@@ -1,15 +1,46 @@
 // All changes here will automatically refresh browser via webpack :)
 
 import { of, from } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, map } from 'rxjs/operators';
 import * as $ from 'jquery';
 
-var requestStream = of('https://api.github.com/users');
+let refreshButton = document.querySelector('.refresh');
 
-var responseStream = requestStream.pipe(flatMap(requestUrl =>
-  from($.getJSON(requestUrl))
-));
+let requestStream = of('https://api.github.com/users');
 
-responseStream.subscribe(response => {
-  console.log(response);
+let responseStream = requestStream.pipe(
+  flatMap(requestUrl => from($.getJSON(requestUrl)))
+);
+
+let createSuggestionStream = (responseStream: any) => {
+  return responseStream.pipe(
+    map(
+      (listUser: any) => listUser[Math.floor(Math.random() * listUser.length)]
+    )
+  );
+};
+
+let suggestion1Stream$ = createSuggestionStream(responseStream);
+let suggestion2Stream$ = createSuggestionStream(responseStream);
+let suggestion3Stream$ = createSuggestionStream(responseStream);
+
+let renderSuggestion = (userData: any, selector: any) => {
+  let element = document.querySelector(selector);
+  let usernameEl = element.querySelector('.username');
+  usernameEl.href = userData.html_url;
+  usernameEl.textContent = userData.login;
+  let imgEl = element.querySelector('img');
+  imgEl.src = userData.avatar_url;
+};
+
+suggestion1Stream$.subscribe((user: any) => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream$.subscribe((user: any) => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream$.subscribe((user: any) => {
+  renderSuggestion(user, '.suggestion3');
 });
