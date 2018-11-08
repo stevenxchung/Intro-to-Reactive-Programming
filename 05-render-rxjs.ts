86fba314058ba1c481415b5e88dbf070836da0a5
Steven <stevenxchung@gmail.com>
2018-11-07 19:49:48 -0500

    Add rendering to DOM after testing

--- 05-render-rxjs.ts
+++ 05-render-rxjs.ts
@@ -1,13 +1,46 @@
 // In this lesson we will render to the DOM (Document Object Model)
 
 import { of, from } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, map } from 'rxjs/operators';
 import * as $ from 'jquery';
 
-var refreshButton = document.querySelector('.refresh');
+let refreshButton = document.querySelector('.refresh');
 
-var requestStream = of('https://api.github.com/users');
+let requestStream = of('https://api.github.com/users');
 
-var responseStream = requestStream.pipe(flatMap(requestUrl =>
-  from($.getJSON(requestUrl))
-));
+let responseStream = requestStream.pipe(
+  flatMap(requestUrl => from($.getJSON(requestUrl)))
+);
+
+let createSuggestionStream = (responseStream: any) => {
+  return responseStream.pipe(
+    map(
+      (listUser: any) => listUser[Math.floor(Math.random() * listUser.length)]
+    )
+  );
+};
+
+let suggestion1Stream$ = createSuggestionStream(responseStream);
+let suggestion2Stream$ = createSuggestionStream(responseStream);
+let suggestion3Stream$ = createSuggestionStream(responseStream);
+
+let renderSuggestion = (userData: any, selector: any) => {
+  let element = document.querySelector(selector);
+  let usernameEl = element.querySelector('.username');
+  usernameEl.href = userData.html_url;
+  usernameEl.textContent = userData.login;
+  let imgEl = element.querySelector('img');
+  imgEl.src = userData.avatar_url;
+};
+
+suggestion1Stream$.subscribe((user: any) => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream$.subscribe((user: any) => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream$.subscribe((user: any) => {
+  renderSuggestion(user, '.suggestion3');
+});
