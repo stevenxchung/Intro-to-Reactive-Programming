b27ea0675288a030669ba1d7a8c160bdad4c0735
Steven <stevenxchung@gmail.com>
2018-11-20 18:53:37 -0500

    Add commentary, logs, and values for initial examples

--- 03-why-rxjs.ts
+++ 03-why-rxjs.ts
@@ -4,11 +4,15 @@
 // Here we cannot specify the dynamic behavior of a value completely at the time of declaration
 let a = 3;
 let b = 10 * a;
-console.log(b);
+console.log(b); // 30
 
+// The value of b is still 30
 a = 4;
+console.log(b) // 30
+
+// b is not a constant so the result has been changed from 30 to 44
 b = 11 * a;
-console.log(b)
+console.log(b) // 44
 
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
