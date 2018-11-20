8179d56fccad1967fdf259ccf5b82ea1973eeae9
Steven <stevenxchung@gmail.com>
2018-11-20 18:50:42 -0500

    Add commentary for initial example

--- 03-why-rxjs.ts
+++ 03-why-rxjs.ts
@@ -1,19 +1,19 @@
 // Why use rxjs?
 // We us RxJS because it allows you to specify the dynamic behavior of a value completely at the time of declaration
 
+// Here we cannot specify the dynamic behavior of a value completely at the time of declaration
 let a = 3;
 let b = 10 * a;
-
 console.log(b);
 
 a = 4;
 b = 11 * a;
-
 console.log(b)
 
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+//
 let streamA = of(3, 4);
 let streamB = streamA.pipe(map(a => 10 * a));
 
