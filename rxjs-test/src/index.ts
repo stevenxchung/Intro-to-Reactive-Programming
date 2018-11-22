4f748ec28dc6e48698ce9923b673727fa319a4dc
Steven <stevenxchung@gmail.com>
2018-11-22 15:24:24 -0500

    Use observable notation

--- rxjs-test/src/index.ts
+++ rxjs-test/src/index.ts
@@ -3,12 +3,12 @@
 import { of, from } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 
-const requestStream = of('https://api.github.com/users');
+const requestStream$ = of('https://api.github.com/users');
 
-let responseStream = requestStream.pipe(
+let responseStream$ = requestStream$.pipe(
   flatMap(requestUrl => from(jQuery.getJSON(requestUrl)))
 );
 
-responseStream.subscribe(response => {
+responseStream$.subscribe(response => {
   console.log(response);
 });
