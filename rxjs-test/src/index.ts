f276925dabd5b04485b75e3115d6a5baa815c83d
Steven <stevenxchung@gmail.com>
2018-11-22 15:30:48 -0500

    Add commentary

--- rxjs-test/src/index.ts
+++ rxjs-test/src/index.ts
@@ -3,12 +3,15 @@
 import { of, from } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 
+// Emit variable amount of values in a sequence, in this case it will be a stream of github users
 const requestStream$ = of('https://api.github.com/users');
 
+// flatMap() or mergeMap() will map request URLs to be an observable and emit values
 let responseStream$ = requestStream$.pipe(
   flatMap(requestUrl => from(jQuery.getJSON(requestUrl)))
 );
 
+// By subscribing to responseStream$ we obtain an array of user objects
 responseStream$.subscribe(response => {
   console.log(response);
 });
