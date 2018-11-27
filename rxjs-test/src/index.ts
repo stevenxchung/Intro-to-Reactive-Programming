07b055fded9bb709da6708a4a77b576c803326d4
Steven <stevenxchung@gmail.com>
2018-11-27 18:49:47 -0500

    Add commentary

--- rxjs-test/src/index.ts
+++ rxjs-test/src/index.ts
@@ -12,19 +12,23 @@ import {
 } from 'rxjs/operators';
 import * as $ from 'jquery';
 
+// Initialize selectors
 const refreshButton = document.querySelector('.refresh');
 const closeButton1 = document.querySelector('.close1');
 const closeButton2 = document.querySelector('.close2');
 const closeButton3 = document.querySelector('.close3');
 
-const refreshClickStream = fromEvent(refreshButton, 'click');
+// Initialize click observables
+const refreshClickStream$ = fromEvent(refreshButton, 'click');
 const close1Clicks = fromEvent(closeButton1, 'click');
 const close2Clicks = fromEvent(closeButton2, 'click');
 const close3Clicks = fromEvent(closeButton3, 'click');
 
+// Initialize observable stream
 const startupRequestStream = of('https://api.github.com/users');
 
-let requestOnRefreshStream = refreshClickStream.pipe(
+// Set requestOnRefreshStream to be an observable of random GitHub users
+let requestOnRefreshStream = refreshClickStream$.pipe(
   map(ev => {
     let randomOffset = Math.floor(Math.random() * 500);
     return 'https://api.github.com/users?since=' + randomOffset;
@@ -36,11 +40,13 @@ let requestOnRefreshStream = refreshClickStream.pipe(
 // merge both data streams
 //s-----a---b------c----->
 
+// Merges startupRequestStream with requestOnRefreshStream and maps values to JSON object
 let responseStream = startupRequestStream.pipe(
   merge(requestOnRefreshStream),
   flatMap(requestUrl => from($.getJSON(requestUrl))),
   tap(val => {
     console.log('In startupRequestStream pipe!');
+    console.log(val)
   }),
   shareReplay(1)
 );
@@ -52,17 +58,19 @@ let responseStream = startupRequestStream.pipe(
 //     merge
 // N---u--N---N-u->
 
+// Function that returns an array of randomly ordered GitHub users
 let getRandomUser = (listUsers: any) => {
   return listUsers[Math.floor(Math.random() * listUsers.length)];
 };
 
+// Returns responseStream merged with refreshClickStream$ and closeClickStream
 let createSuggestionStream = (responseStream: any, closeClickStream: any) => {
   return responseStream.pipe(
     map(
       (listUser: any) => listUser[Math.floor(Math.random() * listUser.length)]
     ),
     startWith(null),
-    merge(refreshClickStream.pipe(map(ev => null))),
+    merge(refreshClickStream$.pipe(map(ev => null))),
     merge(
       closeClickStream.pipe(
         withLatestFrom(responseStream, (x: any, R: any) => getRandomUser(R))
@@ -71,11 +79,12 @@ let createSuggestionStream = (responseStream: any, closeClickStream: any) => {
   );
 };
 
+// Setup suggestionStream$
 let suggestion1Stream$ = createSuggestionStream(responseStream, close1Clicks);
 let suggestion2Stream$ = createSuggestionStream(responseStream, close2Clicks);
 let suggestion3Stream$ = createSuggestionStream(responseStream, close3Clicks);
 
-// Rendering
+// Renders user's name and photo to the DOM
 let renderSuggestion = (suggestedUser: any, selector: any) => {
   let suggestionEl = document.querySelector(selector);
   if (suggestedUser === null) {
@@ -92,6 +101,7 @@ let renderSuggestion = (suggestedUser: any, selector: any) => {
   }
 };
 
+// Takes users from the stream and passes it into renderSuggestion()
 suggestion1Stream$.subscribe((user: any) => {
   renderSuggestion(user, '.suggestion1');
 });
