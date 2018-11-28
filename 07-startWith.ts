5134cd53141c311ab498b1c85bb4d2626f4d3338
Steven <stevenxchung@gmail.com>
2018-11-28 18:45:25 -0500

    Update lesson 7 with commentary and notation

--- 07-startWith.ts
+++ 07-startWith.ts
@@ -2,14 +2,34 @@
 // We fix this by using startWith()
 
 import { of, from, fromEvent } from 'rxjs';
-import { flatMap, map, merge, startWith } from 'rxjs/operators';
+import {
+  flatMap,
+  map,
+  merge,
+  startWith,
+  shareReplay,
+  tap,
+  withLatestFrom
+} from 'rxjs/operators';
 import * as $ from 'jquery';
 
-let refreshButton = document.querySelector('.refresh');
-let refreshClickStream = fromEvent(refreshButton, 'click');
-let startupRequestStream = of('https://api.github.com/users');
+// Initialize selectors
+const refreshButton = document.querySelector('.refresh');
+const closeButton1 = document.querySelector('.close1');
+const closeButton2 = document.querySelector('.close2');
+const closeButton3 = document.querySelector('.close3');
 
-let requestOnRefreshStream = refreshClickStream.pipe(
+// Initialize click observables
+const refreshClickStream$ = fromEvent(refreshButton, 'click');
+const close1Clicks$ = fromEvent(closeButton1, 'click');
+const close2Clicks$ = fromEvent(closeButton2, 'click');
+const close3Clicks$ = fromEvent(closeButton3, 'click');
+
+// Initialize observable stream
+const startupRequestStream$ = of('https://api.github.com/users');
+
+// Set requestOnRefreshStream$ to be an observable of random GitHub users
+let requestOnRefreshStream$ = refreshClickStream$.pipe(
   map(ev => {
     let randomOffset = Math.floor(Math.random() * 500);
     return 'https://api.github.com/users?since=' + randomOffset;
@@ -21,9 +41,15 @@ let requestOnRefreshStream = refreshClickStream.pipe(
 // merge both data streams
 //s-----a---b------c----->
 
-let responseStream = requestOnRefreshStream.pipe(
-  merge(startupRequestStream),
-  flatMap(requestUrl => from($.getJSON(requestUrl)))
+// Merges startupRequestStream$ with requestOnRefreshStream$ and maps values to JSON object
+let responseStream$ = startupRequestStream$.pipe(
+  merge(requestOnRefreshStream$),
+  flatMap(requestUrl => from($.getJSON(requestUrl))),
+  tap(val => {
+    console.log('In startupRequestStream$ pipe!');
+    console.log(val)
+  }),
+  shareReplay(1)
 );
 
 // ----u---------->
@@ -33,27 +59,40 @@ let responseStream = requestOnRefreshStream.pipe(
 //     merge
 // N---u--N---N-u->
 
-let createSuggestionStream = (responseStream: any) => {
-  return responseStream.pipe(
+// Function that returns an array of randomly ordered GitHub users
+let getRandomUser = (listUsers: any) => {
+  return listUsers[Math.floor(Math.random() * listUsers.length)];
+};
+
+// Returns responseStream$ merged with refreshClickStream$ and closeClickStream$
+let createSuggestionStream = (responseStream$: any, closeClickStream$: any) => {
+  return responseStream$.pipe(
     map(
       (listUser: any) => listUser[Math.floor(Math.random() * listUser.length)]
     ),
     startWith(null),
-    merge(refreshClickStream.pipe(map(ev => null)))
+    merge(refreshClickStream$.pipe(map(ev => null))),
+    merge(
+      closeClickStream$.pipe(
+        withLatestFrom(responseStream$, (x: any, R: any) => getRandomUser(R))
+      )
+    )
   );
 };
 
-let suggestion1Stream$ = createSuggestionStream(responseStream);
-let suggestion2Stream$ = createSuggestionStream(responseStream);
-let suggestion3Stream$ = createSuggestionStream(responseStream);
+// Setup suggestionStream$
+let suggestion1Stream$ = createSuggestionStream(responseStream$, close1Clicks$);
+let suggestion2Stream$ = createSuggestionStream(responseStream$, close2Clicks$);
+let suggestion3Stream$ = createSuggestionStream(responseStream$, close3Clicks$);
 
-// Rendering
+// Renders user's name and photo to the DOM
 let renderSuggestion = (suggestedUser: any, selector: any) => {
   let suggestionEl = document.querySelector(selector);
   if (suggestedUser === null) {
-    $(selector).hide()
+    $(selector).hide();
   } else {
-    $(selector).show()
+    // Using vanilla JS did not show for some reason
+    $(selector).show();
     let usernameEl = suggestionEl.querySelector('.username');
     usernameEl.href = suggestedUser.html_url;
     usernameEl.textContent = suggestedUser.login;
@@ -63,6 +102,7 @@ let renderSuggestion = (suggestedUser: any, selector: any) => {
   }
 };
 
+// Takes users from the stream and passes it into renderSuggestion()
 suggestion1Stream$.subscribe((user: any) => {
   renderSuggestion(user, '.suggestion1');
 });
