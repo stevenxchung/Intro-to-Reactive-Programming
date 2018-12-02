5ede788bf57f762cd2c718e045be2fd452b3b307
Steven <stevenxchung@gmail.com>
2018-12-02 18:43:12 -0500

    Update from let to const

--- 05-render-rxjs.ts
+++ 05-render-rxjs.ts
@@ -5,19 +5,19 @@ import { flatMap, map } from 'rxjs/operators';
 import * as $ from 'jquery';
 
 // Selector for refresh element
-let refreshButton = document.querySelector('.refresh');
+const refreshButton = document.querySelector('.refresh');
 
 // Will create an observable of GitHub users
-let requestStream$ = of('https://api.github.com/users');
+const requestStream$ = of('https://api.github.com/users');
 
 // flatMap() is an alias for mergeMap(), see https://www.learnrxjs.io/operators/transformation/mergemap.html
 // Here we flatten the requestStream$ of GitHub users to an observable of GitHub users in JSON
-let responseStream$ = requestStream$.pipe(
+const responseStream$ = requestStream$.pipe(
   flatMap(requestUrl => from($.getJSON(requestUrl)))
 );
 
 // Returns responseStream$ where the users are randomly mapped into an array
-let createSuggestionStream = (responseStream$: any) => {
+const createSuggestionStream = (responseStream$: any) => {
   return responseStream$.pipe(
     map(
       (listUser: any) => listUser[Math.floor(Math.random() * listUser.length)]
@@ -26,17 +26,17 @@ let createSuggestionStream = (responseStream$: any) => {
 };
 
 // Each stream will be an individual observable stream of GitHub users in a random order
-let suggestion1Stream$ = createSuggestionStream(responseStream$);
-let suggestion2Stream$ = createSuggestionStream(responseStream$);
-let suggestion3Stream$ = createSuggestionStream(responseStream$);
+const suggestion1Stream$ = createSuggestionStream(responseStream$);
+const suggestion2Stream$ = createSuggestionStream(responseStream$);
+const suggestion3Stream$ = createSuggestionStream(responseStream$);
 
 // Get name and image URL from the GitHub response
-let renderSuggestion = (userData: any, selector: any) => {
-  let element = document.querySelector(selector);
-  let usernameEl = element.querySelector('.username');
+const renderSuggestion = (userData: any, selector: any) => {
+  const element = document.querySelector(selector);
+  const usernameEl = element.querySelector('.username');
   usernameEl.href = userData.html_url;
   usernameEl.textContent = userData.login;
-  let imgEl = element.querySelector('img');
+  const imgEl = element.querySelector('img');
   imgEl.src = userData.avatar_url;
 };
 
